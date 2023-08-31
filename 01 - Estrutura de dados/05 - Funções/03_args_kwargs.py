69a93e5b59d3eaa619c9506a6874cc99d14bdcc3
Denis Pantarotto <depantarotto@gmail.com>
2023-08-31 17:34:56 -0300

    Ajuste

--- 01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
+++ 01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
@@ -6,6 +6,7 @@ def exibir_poema(data_extenso, *args, **kwargs):
 
 
 exibir_poema(
+    "Sexta-Feira, 31 de agosto de 2023",
     "Zen of Python",
     "Beautiful is better than ugly.",
     "Explicit is better than implicit.",
